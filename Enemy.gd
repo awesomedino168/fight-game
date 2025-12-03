111f38e15dde14bf619a208e03cd7a9b471c41ad
ImJqm <james.harper@woxford.com>
2025-12-02 20:59:28 -0600

    added duploication

--- Enemy.gd
+++ Enemy.gd
@@ -6,10 +6,22 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+func _ready() -> void:
+	
+
+	pass # Replace with function body.
+
+
 
 
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("dupe"):
+		var dupe = self.duplicate()
+		get_parent().add_child.call_deferred(dupe)
+		dupe.position = Vector3(position.x, position.y+10, position.z)
+		print("duplicated")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,7 +35,7 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.x = SPEED * target_vector.x
 	velocity.z = SPEED * target_vector.z
-	print(str(velocity.x) + "    " + str(velocity.z))
+	
 	#print(str(target.global_transform.origin.x))
 	
 	
